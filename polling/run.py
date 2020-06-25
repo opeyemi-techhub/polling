1eaa32d0c257c327ac046b02faaab50127341cad
Brocklesnar <Ogunsanyaopeyemi4@gmail.com>
2020-06-25 07:03:49 +0100

    MY first sitee

---
+++ polling/run.py
@@ -0,0 +1,10 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+import datetime
+from django.utils import timezone
+from .models import Voter
+class runner():
+    print("jjj")
